428fd6a1af14451fb7158ae6d08391618786229b
imikado@gmail.com <imikado@gmail.com>
2023-10-08 19:55:11 +0200

    maj images

--- src/Components/Views/banner.php
+++ src/Components/Views/banner.php
@@ -1,26 +1,31 @@
 <style>
+    .bandeau {
+        text-align: center;
+    }
+
     .bandeau_accueil {
-        background: #037d03;
+        background: #002b00;
+
     }
 
     .bandeau_accueil2 {
         margin-top: 10px;
-        background: #202531;
+        background: #20262c;
     }
 
     .bandeau_accueil3 {
         margin-top: 10px;
         margin-bottom: 10px;
-        background: #a28325;
+        background: #55440e;
     }
 </style>
 
 
-<p class="bandeau_accueil"><img class="responsive-img image" src="css/images/bandeau_accueil.png" />
+<p class="bandeau bandeau_accueil"><img class="responsive-img image" src="css/images/bandeau_accueil.jpg" />
 <p>
 
-<p class="bandeau_accueil2"><img class="responsive-img image" src="css/images/bandeau_accueil2.png" />
+<p class="bandeau bandeau_accueil2"><img class="responsive-img image" src="css/images/bandeau_accueil2.jpg" />
 <p>
 
-<p class="bandeau_accueil3"><img class="responsive-img image" src="css/images/bandeau_accueil3.png" />
+<p class="bandeau bandeau_accueil3"><img class="responsive-img image" src="css/images/bandeau_accueil3.jpg" />
 <p>
